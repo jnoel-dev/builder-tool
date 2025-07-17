1eac36fe0f038e2860ff6f02e277df07e537909c
Saint <saint@cafealpha.dev>
2025-07-16 23:37:35 -0400

    add default text to containers

--- src/components/addableElements/Container/Container.tsx
+++ src/components/addableElements/Container/Container.tsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import FrameBase from '@/components/frameBase/FrameBase';
-import { useFrame } from '@/components/frameManager/FrameManager';
+import React from "react";
+import Box from "@mui/material/Box";
+import FrameBase from "@/components/frameBase/FrameBase";
+import { useFrame } from "@/components/frameManager/FrameManager";
 
 interface ContainerProps {
-  containerType: string;
-  savedName: string;
+	containerType: string;
+	savedName: string;
 }
 
 export default function Container({
   containerType = "vertical",
   savedName,
 }: ContainerProps) {
-  const { frameContainerRefs } = useFrame();
-
+  const { frameContainerRefs, allFrameElements } = useFrame();
+  const hasChildren = allFrameElements[savedName]?.length > 0;
 
   return (
     <Box
       ref={frameContainerRefs[savedName]}
       sx={{
-
         border: '1px solid',
-        display: 'inline-block', 
-  
+        display: 'inline-block',
+        maxWidth: '100%' 
       }}
     >
-      <FrameBase frameName={savedName} disableElementControlsForChildren={true}/>
+      <FrameBase frameName={savedName} disableElementControlsForChildren={true} />
+      {!hasChildren && (
+        <Box
+          sx={{
+            wordBreak: 'break-word',
+            textAlign: 'center',
+            width: 200
+          }}
+        >
+          Add elements here by selecting container in Elements menu
+        </Box>
+      )}
     </Box>
   );
 }
+
