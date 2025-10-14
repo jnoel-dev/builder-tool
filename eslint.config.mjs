6c468a1145958e4473eaa9e6c393af6a60a382b7
Saint <saint@cafealpha.dev>
2025-10-14 13:58:51 -0400

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,8 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import unusedImports from "eslint-plugin-unused-imports";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default [
   {
-    ignores: ["node_modules/", ".next/", "public/", "next-env.d.ts"],
-  },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+    files: ["src/**/*.{ts,tsx}"],
     plugins: { "unused-imports": unusedImports },
     rules: {
       "no-unused-vars": "off",
